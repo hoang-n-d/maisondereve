d05811a8ac9bdd146671d92cb02af57cccb89f3a
Hoang14127 <ngusimeri@gmail.com>
2024-06-15 17:34:43 +0700

    fix-ui-2

--- Repository/obj/Debug/net6.0/Repository.AssemblyInfo.cs
+++ Repository/obj/Debug/net6.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e9971b8019a55d349a553042bc8a0cedc81dade")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+985df5f9f99cb71fc04e9e4c0526d83c119f6cac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
